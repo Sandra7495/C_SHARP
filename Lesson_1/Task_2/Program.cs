0975a55adff5c0a02cc4bd7a4ff7780c5a49d742
Aleksandra <Sandra_74_95@mail.ru>
2022-09-01 23:42:57 +0500

    Решение второй задачи

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите 1е число ");
+string number1 = Console.ReadLine();
+int num1 = Convert.ToInt32(number1);
+
+Console.Write("Введите 2е число ");
+string number2 = Console.ReadLine();
+int num2 = Convert.ToInt32(number2);
+
+Console.Write("Введите 3е число ");
+string number3 = Console.ReadLine();
+int num3 = Convert.ToInt32(number3);
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+      Console.Write("Максимальное число");
+      Console.WriteLine (num1);
+    }
+    else
+    {
+      Console.Write("Максимальное число");
+      Console.WriteLine (num3); 
+    }
+}
+else
+{
+     if (num2 > num3)
+     {
+       Console.Write("Максимальное число");
+      Console.WriteLine (num2); 
+     }
+     else
+    {
+      Console.Write("Максимальное число");
+      Console.WriteLine (num3); 
+    }
+}
