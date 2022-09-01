beeeabfdbae409de8650f1fcad52e7167aae6b9a
Aleksandra <Sandra_74_95@mail.ru>
2022-09-01 23:02:03 +0500

    Решение первой задачи

---
+++ Lesson_1/Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите 1е число ");
+string number1 = Console.ReadLine();
+int num1 = Convert.ToInt32(number1);
+
+Console.Write("Введите 2е число ");
+string number2 = Console.ReadLine();
+int num2 = Convert.ToInt32(number2);
+
+if ( num1 > num2 )
+{
+    Console.Write("Большее ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("Большее ");
+    Console.WriteLine(num2);
+}
+
