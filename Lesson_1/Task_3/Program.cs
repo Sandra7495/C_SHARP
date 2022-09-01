df2be0819d9deea982b9c9c77e9ffc9b5ab2fcbb
Aleksandra <Sandra_74_95@mail.ru>
2022-09-01 23:56:21 +0500

    Решение третьей  задачи

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+
+if (num % 2 == 0)
+ {
+    Console.WriteLine("число четное");
+ }
+else
+{
+   Console.WriteLine("Число нечетное"); 
+}
+
+
+
+
