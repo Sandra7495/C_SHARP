3a17b7f00c13125332f1aef77fede455b1a6ad05
Aleksandra <Sandra_74_95@mail.ru>
2022-09-02 00:17:11 +0500

    Решение задачи под *

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+
+int count = 1;
+
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count ++;
+}
