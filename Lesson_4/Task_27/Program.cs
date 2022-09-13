028d7a640f3de427ee27fccbbb5dbe7796090b9a
Aleksandra <Sandra_74_95@mail.ru>
2022-09-13 21:46:56 +0500

    Решение задачи № 27

--- Lesson_4/Task_27/Program.cs
+++ Lesson_4/Task_27/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+while (number != 0)
+{
+    int i = number % 10;
+    sum = sum + i;
+    number = number / 10;
+}
+Console.WriteLine (sum);
