87153c6ee34cd499fdb870aa127aba9ba42ab87a
Aleksandra <Sandra_74_95@mail.ru>
2022-09-13 21:07:43 +0500

    Решение задачи № 25

---
+++ Lesson_4/Task_25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число А: ");
+double A = double.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+double B = double.Parse(Console.ReadLine());
+double d = Math.Pow(A,B);
+d = Math.Round(d,2);
+Console.WriteLine($"d = {d} ");
