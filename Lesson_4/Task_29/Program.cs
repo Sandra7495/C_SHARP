aecbdea29b879682fbbe71cf833197aeb84ef824
Aleksandra <Sandra_74_95@mail.ru>
2022-09-15 21:43:03 +0500

    Решение задачи № 29

--- Lesson_4/Task_29/Program.cs
+++ Lesson_4/Task_29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int[] array = GetArray(8,1,10);
+WriteLine(String.Join (" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
---
+++ Lesson_5/Seminar_5/Program.cs
@@ -0,0 +1,25 @@
+﻿**Задача 37:** Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+[1 2 3 4 5] -> 5 8 3
+
+[6 7 3 6] -> 36 21
+
+Clear();
+Write("Введите элементы массива через пробел: ");
+string elements = ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+WriteLine(String.Join(" ", ResultArray(baseArray)));
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
