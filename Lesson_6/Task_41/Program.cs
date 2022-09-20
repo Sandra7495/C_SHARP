50edb7f7a1869805b1aef9afb044efe45e8f7a9c
Aleksandra <Sandra_74_95@mail.ru>
2022-09-20 23:31:07 +0500

    Решение задач № 41, № 43

---
+++ Lesson_6/Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите числа через запятую: ");
+string[] StrArrNumbers = ReadLine().Split(",");
+int count = CountNumbers(StrArrNumbers);
+WriteLine($"Колличество чисел больше нуля = {count}");
+
+int CountNumbers(string[] StrArrNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < StrArrNumbers.Length; i++)
+    {
+        if (int.Parse(StrArrNumbers[i]) > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
---
+++ Lesson_6/Task_43/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите значения b1, k1, b2 и k2 через пробел: ");
+double[] Values = Array.ConvertAll(ReadLine().Split(), double.Parse);
+double x = (-Values[2] + Values[0])/(-Values[1] + Values[3]);
+double y = Values[3] * x + Values[2];
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}"); 
+
+
