4a333b699798890e0b79ef76ccb1269a67a3c5b2
Aleksandra <Sandra_74_95@mail.ru>
2022-10-02 23:22:40 +0500

    решение задач № 64, № 66, № 68

---
+++ Lesson_9/Task_64/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+
+Console.WriteLine("Введите число от 1 до 1000:");
+int number = int.Parse(Console.ReadLine());
+interNumbers(number);
+
+void interNumbers (int number)
+{
+    if (number == 0) return;
+    Console.Write(number + " ");
+    interNumbers (number - 1);
+}
+
---
+++ Lesson_9/Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+Console.WriteLine("Введите первое число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите последнее число N:");
+int numberN = int.Parse(Console.ReadLine());
+GapNumberSum(numberM, numberN, 0);
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + numberM;
+    numberM++;
+    GapNumberSum(numberM, numberN, sum);
+}
+
